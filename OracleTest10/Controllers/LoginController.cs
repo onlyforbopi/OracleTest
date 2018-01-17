30fd35a894bfa0e4812a5c2fd0f49f20cd6cbe25
p.doulgeridis <p.doulgeridis@DEHHOL-WS134>
2018-01-17 09:19:34 +0200

    Add project files.

---
+++ OracleTest10/Controllers/LoginController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OracleTest10.Models;
+
+namespace OracleTest10.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        // This will redirect us to main login view : Index
+        // View will be stored into Views/Login/Index.cshtml
+        // No parameter is passed.
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        // Specify PostURL for LoginForm
+        [HttpPost]
+        public ActionResult Authorize(OracleTest10.Models.USR userModel)
+        {
+            // Here we handle login sumissions
+            // The submission form values will be of the type class 
+            //       OracleTest10.Models.USR
+            //
+            // This is the class defined by ADO in User.cs
+
+
+            // We find class name for our db from DBNAME.Context.cs
+            // The following command instantiates a db of that class
+            using (OREntities db = new OREntities())
+            {
+
+                // If there's any user with that username/pass the userDetails will fill, success
+                // Standard LINQ command: <db>.<tablename>.Where()
+                // Performs a query for values AM = InputAM and PASSWORD = InputPASSWORD
+                // FirstOrDefault returns the first entry, or default if they found none.
+                var userDetails = db.USRs.Where(x => x.ID == userModel.ID && x.PASSWORD == userModel.PASSWORD).FirstOrDefault();
+                
+                // if its null
+                if (userDetails == null)
+                {
+                    // We ve added a property in the User.cs file to handle error message.
+                    // Property added in User.cs : 
+                    userModel.LoginErrorMessage = "Wrong Credentials";
+                    return View("Index", userModel);
+                }
+                else
+                {
+                    // If login is succesfull, we set several parameters
+                    // as session parameters (they will last throughout the session)
+                    // This is used in conjuction with a razor block at the beginning
+                    // of every other view (but for login), where we will check if
+                    // Session["userID"] is null, and if so, we ll redirect to the main page.
+                    Session["userID"] = userDetails.ID;
+                    Session["Name"] = userDetails.FIRST_NAME;
+                    Session["Surname"] = userDetails.LAST_NAME;
+
+                    // Here, we redirect to our main page, controller: Other, action: Index
+                    //return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Other");
+                }
+                //return View();
+            }
+
+        }
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
---
+++ OracleTest10/Controllers/OtherController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OracleTest10.Models;
+using System.Net;
+
+namespace OracleTest10.Controllers
+{
+    public class OtherController : Controller
+    {
+        // GET: Other
+        public ActionResult Index()
+        {
+
+            //Models.MailTrackingEntities gvbd = new Models.MailTrackingEntities();
+            Models.OREntities gvbd = new Models.OREntities();
+
+            // LINQ QUERY ???
+            var SchemeList = (from scheme in gvbd.MAINs select scheme).ToList();
+
+            return View(SchemeList);
+
+
+            //return View();
+        }
+
+
+        // ActionResult = Result of action method 
+        // Returns View(singleuser) = View with model of single user passed to it.
+        // Model of single user is a table row with the same fields. Model can have more than one rows.
+        // Takes id input.
+        public ActionResult Snapshot(decimal id)
+        {
+            if (id.Equals(null))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Models.OREntities gvbd = new Models.OREntities();
+            MAIN singleuser = gvbd.MAINs.Find(id);
+            if (singleuser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(singleuser);
+
+        }
+
+
+
+        // GET: Other/Details/5
+        public ActionResult Details(string id)
+        {
+            return View();
+        }
+
+        // GET: Other/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Other/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Other/Edit/5
+        /// <summary>
+        /// ///////
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET METHOD
+        // Function : Brings up the edit form URL.
+        // Input: string ID 
+        // Output : EditView with model supplied, new user
+        public ActionResult Edit(decimal id)
+        {
+            if (id.Equals(null))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Models.OREntities gvbd = new Models.OREntities();
+            MAIN newuser = gvbd.MAINs.Find(id);
+            if (newuser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(newuser);
+        }
+
+        // POST: Other/Edit/5
+        [HttpPost]
+        public ActionResult Edit(string id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Other/Delete/5
+        public ActionResult Delete(string id)
+        {
+            return View();
+        }
+
+        // POST: Other/Delete/5
+        [HttpPost]
+        public ActionResult Delete(string id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
